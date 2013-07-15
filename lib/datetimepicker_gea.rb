526a5f885d30dc35ce9f1088f1a58f6fbdf548c3
Emile Victor <emile@gea-interactive.com.au>
2013-07-15 10:14:13 +1000

    Fixing an issue with applicationhelper

--- lib/datetimepicker_gea.rb
+++ lib/datetimepicker_gea.rb
@@ -1,4 +1,5 @@
-require "datetimepicker_gea/application_helper_hook"
+#require "datetimepicker_gea/application_helper_hook"
+require 'datetimepicker_gea/railtie' if defined?(Rails)
 
 
 module DatetimepickerGea
